adbd682398fefa1ac2ac721f74031254cdcb0ce2
dashIink <71524804+dashIink@users.noreply.github.com>
2023-02-09 13:25:58 -0500

    CD implementation

--- shell.c
+++ shell.c
@@ -3,7 +3,41 @@
 #include <string.h>
 #include <stdbool.h>
 #include <unistd.h>
+#include <limits.h>
 
-int main(){
+char *param[5];
+char *func;
+
+void getCommand(){
+	char input[256];
+	char cwd[1000];
+	getcwd(cwd, 1000);
+	strcat(cwd,": ");
+	printf("%s", cwd);
+	scanf(" %[^\n]s", input);
+	char *token = strtok(input, " ");
+	
+	
+	int i = 0;
+	func = token;
+	while(token){
+    		token = strtok(NULL, " ");
+    		param[i++] = token;
+	}
+	
+	
 	
 }
+
+int main(int argc, char *argv[]){
+	getCommand();
+	while(strcmp(func, "quit") != 0){
+	
+		if(strcmp(func, "cd") == 0){
+			chdir(param[0]);
+			
+		}
+	getCommand();
+	}
+	
+}	
